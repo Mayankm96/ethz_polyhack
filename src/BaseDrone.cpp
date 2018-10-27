ed9815a90c74e9989229802b301950cd9bcba5a4
Mayank Mittal <mayankm.iitk@gmail.com>
2018-10-27 18:20:39 +0200

    basedrone added

---
+++ src/BaseDrone.cpp
@@ -0,0 +1,148 @@
+#include <BaseDrone.h>
+
+// constructor for class
+BaseDrone::BaseDrone(std::string connection_url) : dc_()
+{
+  connection_url_ = connection_url;
+}
+
+// constructor for class
+BaseDrone::BaseDrone() : dc_()
+{
+  connection_url_ = "udp://:14540";
+}
+
+// check connection with gazebo
+bool BaseDrone::check_connection()
+{
+  connection_result_ = dc_.add_any_connection(connection_url_);
+
+  if (connection_result_ != ConnectionResult::SUCCESS)
+  {
+      std::cout << ERROR_CONSOLE_TEXT
+                << "Connection failed: " << connection_result_str(connection_result_)
+                << NORMAL_CONSOLE_TEXT << std::endl;
+      return 1;
+  }
+
+  return 0;
+}
+
+// discover the drone on gazebo
+bool BaseDrone::discover_system()
+{
+  bool discovered_system = false;
+
+  std::cout << "Waiting to discover system..." << std::endl;
+
+  dc_.register_on_discover([&discovered_system](uint64_t uuid) {
+      std::cout << "Discovered system with UUID: " << uuid << std::endl;
+      discovered_system = true;
+  });
+
+  // We usually receive heartbeats at 1Hz, therefore we should find a system after around 2
+  // seconds.
+  sleep_for(seconds(2));
+
+  if (!discovered_system) {
+      std::cout << ERROR_CONSOLE_TEXT << "No system found, exiting." << NORMAL_CONSOLE_TEXT
+                << std::endl;
+      return 1;
+  }
+
+  // We don't need to specify the UUID if it's only one system anyway.
+  // If there were multiple, we could specify it with:
+  // dc.system(uint64_t uuid);
+  System &system = dc_.system();
+
+  telemetry_ = std::make_shared<Telemetry>(system);
+  action_ = std::make_shared<Action>(system);
+
+  return 0;
+}
+
+// enable listening to drone
+bool BaseDrone::enable_telemtry()
+{
+  // We want to listen to the altitude of the drone at 1 Hz.
+  const Telemetry::Result set_rate_result = telemetry_->set_rate_position(1.0);
+
+  if (set_rate_result != Telemetry::Result::SUCCESS)
+  {
+      std::cout << ERROR_CONSOLE_TEXT
+                << "Setting rate failed:" << Telemetry::result_str(set_rate_result)
+                << NORMAL_CONSOLE_TEXT << std::endl;
+      return 1;
+  }
+
+  // Set up callback to monitor altitude while the vehicle is in flight
+  telemetry_->position_async([](Telemetry::Position position)
+  {
+      std::cout << TELEMETRY_CONSOLE_TEXT // set to blue
+                << "Altitude: " << position.relative_altitude_m << " m"
+                << NORMAL_CONSOLE_TEXT // set to default color again
+                << std::endl;
+  });
+
+  return 0;
+}
+
+// arm the drone
+bool BaseDrone::enable_arm()
+{
+  // Check if vehicle is ready to arm
+  while (telemetry_->health_all_ok() != true) {
+      std::cout << "Vehicle is getting ready to arm" << std::endl;
+      sleep_for(seconds(1));
+  }
+
+  // Arm vehicle
+  std::cout << "Arming..." << std::endl;
+  const ActionResult arm_result = action_->arm();
+
+  if (arm_result != ActionResult::SUCCESS)
+  {
+      std::cout << ERROR_CONSOLE_TEXT << "Arming failed:" << action_result_str(arm_result)
+                << NORMAL_CONSOLE_TEXT << std::endl;
+      return 1;
+  }
+
+  return 0;
+}
+
+
+// takeoff drone
+bool BaseDrone::takeoff()
+{
+  // Take off
+  std::cout << "Taking off..." << std::endl;
+  const ActionResult takeoff_result = action_->takeoff();
+  if (takeoff_result != ActionResult::SUCCESS)
+  {
+      std::cout << ERROR_CONSOLE_TEXT << "Takeoff failed:" << action_result_str(takeoff_result)
+                << NORMAL_CONSOLE_TEXT << std::endl;
+      return 1;
+  }
+
+  return 0;
+}
+
+// land drone
+bool BaseDrone::landing()
+{
+  std::cout << "Landing..." << std::endl;
+  const ActionResult land_result = action_->land();
+  if (land_result != ActionResult::SUCCESS) {
+      std::cout << ERROR_CONSOLE_TEXT << "Land failed:" << action_result_str(land_result)
+                << NORMAL_CONSOLE_TEXT << std::endl;
+      return 1;
+  }
+
+  // Check if vehicle is still in air
+  while (telemetry_->in_air()) {
+      std::cout << "Vehicle is landing..." << std::endl;
+      sleep_for(seconds(1));
+  }
+  std::cout << "Landed!" << std::endl;
+  return 0;
+}
--- src/takeoff_and_land.cpp
+++ src/takeoff_and_land.cpp
@@ -1,23 +1,4 @@
-//
-// Simple example to demonstrate how to use DroneCore.
-//
-// Author: Julian Oes <julian@oes.ch>
-
-#include <chrono>
-#include <cstdint>
-#include <dronecore/action.h>
-#include <dronecore/dronecore.h>
-#include <dronecore/telemetry.h>
-#include <iostream>
-#include <thread>
-
-using namespace dronecore;
-using namespace std::this_thread;
-using namespace std::chrono;
-
-#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
-#define TELEMETRY_CONSOLE_TEXT "\033[34m" // Turn text on console blue
-#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console colour
+#include <BaseDrone.h>
 
 void usage(std::string bin_name)
 {
@@ -31,109 +12,34 @@ void usage(std::string bin_name)
 
 int main(int argc, char **argv)
 {
-    DroneCore dc;
+    // take input string
     std::string connection_url;
-    ConnectionResult connection_result;
-
-    bool discovered_system = false;
     if (argc == 2) {
         connection_url = argv[1];
-        connection_result = dc.add_any_connection(connection_url);
     } else {
         usage(argv[0]);
         return 1;
     }
 
-    if (connection_result != ConnectionResult::SUCCESS) {
-        std::cout << ERROR_CONSOLE_TEXT
-                  << "Connection failed: " << connection_result_str(connection_result)
-                  << NORMAL_CONSOLE_TEXT << std::endl;
-        return 1;
-    }
+    BaseDrone drone(connection_url);
 
-    std::cout << "Waiting to discover system..." << std::endl;
-    dc.register_on_discover([&discovered_system](uint64_t uuid) {
-        std::cout << "Discovered system with UUID: " << uuid << std::endl;
-        discovered_system = true;
-    });
+    if(drone.check_connection())
+      return 1;
 
-    // We usually receive heartbeats at 1Hz, therefore we should find a system after around 2
-    // seconds.
-    sleep_for(seconds(2));
+    if(drone.discover_system())
+      return 1;
 
-    if (!discovered_system) {
-        std::cout << ERROR_CONSOLE_TEXT << "No system found, exiting." << NORMAL_CONSOLE_TEXT
-                  << std::endl;
-        return 1;
-    }
+    if(drone.enable_telemtry())
+      return 1;
 
-    // We don't need to specify the UUID if it's only one system anyway.
-    // If there were multiple, we could specify it with:
-    // dc.system(uint64_t uuid);
-    System &system = dc.system();
-
-    auto telemetry = std::make_shared<Telemetry>(system);
-    auto action = std::make_shared<Action>(system);
-
-    // We want to listen to the altitude of the drone at 1 Hz.
-    const Telemetry::Result set_rate_result = telemetry->set_rate_position(1.0);
-    if (set_rate_result != Telemetry::Result::SUCCESS) {
-        std::cout << ERROR_CONSOLE_TEXT
-                  << "Setting rate failed:" << Telemetry::result_str(set_rate_result)
-                  << NORMAL_CONSOLE_TEXT << std::endl;
-        return 1;
-    }
-
-    // Set up callback to monitor altitude while the vehicle is in flight
-    telemetry->position_async([](Telemetry::Position position) {
-        std::cout << TELEMETRY_CONSOLE_TEXT // set to blue
-                  << "Altitude: " << position.relative_altitude_m << " m"
-                  << NORMAL_CONSOLE_TEXT // set to default color again
-                  << std::endl;
-    });
-
-    // Check if vehicle is ready to arm
-    while (telemetry->health_all_ok() != true) {
-        std::cout << "Vehicle is getting ready to arm" << std::endl;
-        sleep_for(seconds(1));
-    }
-
-    // Arm vehicle
-    std::cout << "Arming..." << std::endl;
-    const ActionResult arm_result = action->arm();
-
-    if (arm_result != ActionResult::SUCCESS) {
-        std::cout << ERROR_CONSOLE_TEXT << "Arming failed:" << action_result_str(arm_result)
-                  << NORMAL_CONSOLE_TEXT << std::endl;
-        return 1;
-    }
-
-    // Take off
-    std::cout << "Taking off..." << std::endl;
-    const ActionResult takeoff_result = action->takeoff();
-    if (takeoff_result != ActionResult::SUCCESS) {
-        std::cout << ERROR_CONSOLE_TEXT << "Takeoff failed:" << action_result_str(takeoff_result)
-                  << NORMAL_CONSOLE_TEXT << std::endl;
-        return 1;
-    }
+    if(drone.takeoff())
+      return 1;
 
     // Let it hover for a bit before landing again.
     sleep_for(seconds(10));
 
-    std::cout << "Landing..." << std::endl;
-    const ActionResult land_result = action->land();
-    if (land_result != ActionResult::SUCCESS) {
-        std::cout << ERROR_CONSOLE_TEXT << "Land failed:" << action_result_str(land_result)
-                  << NORMAL_CONSOLE_TEXT << std::endl;
-        return 1;
-    }
-
-    // Check if vehicle is still in air
-    while (telemetry->in_air()) {
-        std::cout << "Vehicle is landing..." << std::endl;
-        sleep_for(seconds(1));
-    }
-    std::cout << "Landed!" << std::endl;
+    if(drone.landing())
+      return 1;
 
     // We are relying on auto-disarming but let's keep watching the telemetry for a bit longer.
     sleep_for(seconds(3));
