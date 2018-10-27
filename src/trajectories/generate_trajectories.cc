0f47f85f1235959b234c02af6dd24900fd87dbfd
Francesco Milano <francescomilano172@gmail.com>
2018-10-27 23:51:43 +0200

    remove pointers, add output values

--- src/trajectories/generate_trajectories.cc
+++ src/trajectories/generate_trajectories.cc
@@ -1,11 +1,13 @@
 #include <trajectories/generate_trajectories.h>
 
-void trajectories::generate_straight_line(std::vector <Vector3r> *out_waypoints){
+std::vector <Vector3r> trajectories::generate_straight_line(){
   double x = 0;
+  std::vector <Vector3r> out_waypoints;
   Vector3r app_waypoint (0,0,0);
   while(x < 5.0){
     app_waypoint.set(x,0,0);
-    out_waypoints->push_back(app_waypoint);
+    out_waypoints.push_back(app_waypoint);
     x += 0.1;
   }
+  return out_waypoints;
 }
