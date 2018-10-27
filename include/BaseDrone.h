09ca17141321e17afd7c3290b2580d5319ae8b1b
Mayank Mittal <mayankm.iitk@gmail.com>
2018-10-27 18:20:39 +0200

    basedrone added

---
+++ include/BaseDrone.h
@@ -0,0 +1,54 @@
+#ifndef __BaseDrone_H_INCLUDED__
+#define __BaseDrone_H_INCLUDED__
+
+#include <chrono>
+#include <cstdint>
+#include <dronecore/action.h>
+#include <dronecore/dronecore.h>
+#include <dronecore/telemetry.h>
+#include <iostream>
+#include <thread>
+
+using namespace dronecore;
+using namespace std::this_thread;
+using namespace std::chrono;
+
+#define ERROR_CONSOLE_TEXT "\033[31m" // Turn text on console red
+#define TELEMETRY_CONSOLE_TEXT "\033[34m" // Turn text on console blue
+#define NORMAL_CONSOLE_TEXT "\033[0m" // Restore normal console colour
+
+class BaseDrone
+{
+  private:
+    DroneCore dc_;
+    std::string connection_url_;
+    ConnectionResult connection_result_;
+    std::shared_ptr<Telemetry> telemetry_;
+    std::shared_ptr<Action> action_;
+
+  public:
+    // constructor for class to check connection
+    BaseDrone(std::string connection_url);
+    BaseDrone();
+
+    // check connection with gazebo
+    bool check_connection();
+
+    // discover the drone on gazebo
+    bool discover_system();
+
+    // enable listening to drone
+    bool enable_telemtry();
+
+    // arm the drone
+    bool enable_arm();
+
+    // takeoff drone
+    bool takeoff();
+
+    // land drone
+    bool landing();
+
+};
+
+#endif
