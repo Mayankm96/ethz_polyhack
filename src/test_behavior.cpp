f0cbd649d0221ea4ffb6f8a147d70156b50bfff1
Mayank Mittal <mayankm.iitk@gmail.com>
2018-10-28 05:40:56 +0100

    added separate function for tornado task

--- src/test_behavior.cpp
+++ src/test_behavior.cpp
@@ -45,6 +45,7 @@ int main(int argc, char **argv)
     sleep_for(seconds(10));
 
     std::vector <Vector3r> path;
+
     // path = trajectories::generate_straight_line(Vector3r(5, 5, -5));
     // path = trajectories::generate_helical_path(10, 3, 3, 5, 500, Vector3r(0, 0, -6));
     // path = trajectories::generate_epicycloidal_path(3.0, 2.0, 1000, Vector3r(0, 0, -3));
